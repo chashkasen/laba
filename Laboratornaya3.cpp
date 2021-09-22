ae1123677f0dda6f5f457135a1237a9e02cd31e3
chashkasen <90409564+chashkasen@users.noreply.github.com>
2021-09-22 19:46:08 +0200

    Add files via upload

---
+++ Laboratornaya3.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using std::cin; using std::cout; using std::endl;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int n,a, num;
+	int x = 1;
+	int min = INT_MAX;
+	cout << "Ввод кол-ва элементов последовательности: ";
+	cin >> n;
+	cout << "Вывод последовательности: ";
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> a;
+		
+		if (a % 10 == 2 or a % 10 == 4)
+		{
+			x = x * a;	
+		
+			if (a < min)
+				{
+					min = a;
+					num = i;
+				}
+			}
+		
+	}
+	cout << "Произведение чисел с 2 или 4 на конце: " << x << ", из них наименьшее число - " << min << ", стоящее в последовательности под номером " << num << endl;
+	return 0;
+
+}
+
